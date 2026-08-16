955cb50bc533c1e311e2f2d9258a8298101f3048
chinnu2205 <sumisowmya5@gmail.com>
2026-08-16 16:28:40 +0530

    Initial commit

---
+++ day02.py
@@ -0,0 +1,28 @@
+
+#HomeWork
+#Task 1: Tokens & Statements
+#Create a Python program that stores your name, age, and city in three variables and prints them.
+#Example output:
+#Name: Rahul
+#Age: 20
+#Name = 'sowmya'
+#age = 23
+#city = 'vizag'
+#print((Name))
+#print((age))
+#print((city))
+#Task 2: Identifiers
+#Create three variables using proper identifiers:
+	#• Student name
+	#• Student age
+	#• Student marks
+#Print all three values.
+#Challenge: Create one invalid identifier as a comment and explain why it is invalid.
+name = 'venkat'
+age = 30
+marks = 98
+print(( name ))
+print(( age ))
+print(( marks ))
+#Task 3: Single-Line Comments
+# python is high level language
