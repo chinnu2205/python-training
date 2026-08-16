fc7b6d7404accc7e03beee86eb0b2d074689bb7d
chinnu2205 <sumisowmya5@gmail.com>
2026-08-16 22:08:44 +0530

    Initial commit

---
+++ day03.py
@@ -0,0 +1,55 @@
+#10min Task
+#Datatypes
+#Create one variable for each of the 11 Python data types below:
+#int, float, complex, bool, NoneType, string, range, list, tuple, set, dict
+#For each variable:
+	#1. Store a suitable value.
+	#2. Use type() to print its data type.
+#integer
+#a = 35
+#print (type(a))
+#b = 3.5
+#c = 3+4j
+#d = False
+#g = 32 
+#v = None
+#print (type(b))
+#print (type(c))
+#print (type(d))
+#print (type(g))
+#print (type(v))
+#Type conversion
+#Now convert
+#int to float
+#a = 32
+#a = float(32)
+#print(type(a))
+#float to int
+#b = 3.5
+#c = int(b)
+#print(type(c))
+
+#int to str
+#a = 32
+#c = str(a)
+#print(type(c))
+#str to int
+d = '25'
+f = int(d)
+print(type(f))
+#list to tuple
+p = (1,2,3)
+f = tuple(p)
+print(type(f))
+#tuple to list
+u = (5,1,8)
+f = list(u)
+print(type(f))
+#list to set
+j = [7,8,9]
+q = set(j)
+print(type(q))
+#range to list
+n = range(1,5,2)
+t = list(n)
+print(type(t))
